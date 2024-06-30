8a7278e9b51bdfa2dbbadf011844f4c6834275e5
Amir Taheri <amir.taheri.web@protonmail.com>
2024-06-30 17:20:02 +0330

    Initial commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,36 @@
 import type { Config } from "tailwindcss";
 
-const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+const config = {
+  content: ["./src/**/*.{ts,tsx}"],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        main: {
+          1: "#000000",
+          2: "#610094",
+          3: "#150050",
+          4: "#3F0071",
+        },
+      },
+      keyframes: {
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
       },
     },
   },
-  plugins: [],
-};
+  prefix: "",
+  darkMode: ["class"],
+  plugins: [require("tailwindcss-animate")],
+} satisfies Config;
+
 export default config;
