e751d6cc2405aa1e3fb270aba6316e84d56764a9
Amir Taheri <amir.taheri.web@protonmail.com>
2024-07-01 11:14:12 +0330

    fix: header title updated

--- src/layouts/top.tsx
+++ src/layouts/top.tsx
@@ -1,18 +1,21 @@
 import Image from "next/image";
 import logo from "@/public/assets/logo.png";
+import Link from "next/link";
 
 const TopNav = () => {
   return (
     <header className="flex justify-between gap-8 items-center px-8 h-[90px] fixed top-0 left-0 w-full z-40 bg-dark-2">
-      <h1 className="flex items-center gap-2 text-2xl font-extrabold">
-        <Image
-          src={logo}
-          alt="header logo"
-          width={30}
-          height={30}
-          className="h-auto"
-        />
-        Netalk
+      <h1 className="text-2xl font-extrabold">
+        <Link href="/" className="flex items-center gap-2">
+          <Image
+            src={logo}
+            alt="header logo"
+            width={30}
+            height={30}
+            className="h-auto"
+          />
+          Netalk
+        </Link>
       </h1>
 
       <div>
