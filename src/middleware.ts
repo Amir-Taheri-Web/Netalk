2a9e841777ae1e79c1001f2dcdd02cc254a73235
Amir Taheri <amir.taheri.web@protonmail.com>
2024-07-01 16:09:34 +0330

    chore: clerk configuration completed

---
+++ src/middleware.ts
@@ -0,0 +1,13 @@
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+
+const protectedRoutes = createRouteMatcher(["/"]);
+
+export default clerkMiddleware((auth, req) => {
+  if (protectedRoutes(req)) {
+    auth().protect();
+  }
+});
+
+export const config = {
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+};
